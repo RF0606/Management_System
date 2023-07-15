7435cc2f988adb5b1207d93dfa64c56969925958
RF0606 <15rf17@queensu.ca>
2023-07-15 12:12:12 -0400

    update

--- src/main/java/com/peter/ManagementProjectApplication.java
+++ src/main/java/com/peter/ManagementProjectApplication.java
@@ -2,6 +2,7 @@ package com.peter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
